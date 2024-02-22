baab28625719b087cab5d7617ef013287e2af876
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-02-22 11:34:48 -0500

    Packer Actions added

---
+++ packer/user-creation.sh
@@ -0,0 +1,5 @@
+sudo groupadd -f csye6225
+sudo useradd -m -g csye6225 -s /usr/sbin/nologin csye6225
+sudo chown -R csye6225:csye6225 /opt
+sudo cp /tmp/csye6225.service /lib/systemd/system/csye6225.service
+
---
+++ packer/user-start.sh
@@ -0,0 +1,4 @@
+sudo systemctl daemon-reload
+sudo systemctl enable csye6225
+sudo systemctl start csye6225
+sudo systemctl status csye6225
