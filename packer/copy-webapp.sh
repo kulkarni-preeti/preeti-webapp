fda72ebc0135a50c320b6b1190d46c4a162a225e
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-02-22 12:41:31 -0500

    node version install

--- packer/copy-webapp.sh
+++ packer/copy-webapp.sh
@@ -5,7 +5,7 @@ sudo chmod -R 755 /opt
 sudo cp /tmp/webapp.zip /opt/
 cd /opt || exit
 sudo unzip webapp.zip -d /opt/webapp
-sudo cp /tmp/.env /opt/webapp/.env
+sudo cp /tmp/.env /opt/webapp
 cd /opt/webapp
 sudo npm install
 sudo npm test
