95dd9e49e99a811f765e756d5c2c2f7f14da7f9e
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-02-26 15:07:20 -0500

    Update create-account.test.js

--- tests/create-account.test.js
+++ tests/create-account.test.js
@@ -33,7 +33,7 @@ describe("Create an Account", () => {
     request(app)
       .get("/vi/user")
       .set("Authorization", authHeader)
-      .expect(200)
+      .expect(400)
       .end((err, res) => {
         if (err) return done(err);
         expect(res.body).to.deep.equal(createdUser);
