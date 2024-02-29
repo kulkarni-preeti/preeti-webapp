a9db47d91da1539d6d0ac14ec5396fdf8961e74e
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-02-29 12:10:34 -0500

    missing webapp added

--- packer/copy-webapp.sh
+++ packer/copy-webapp.sh
@@ -4,6 +4,6 @@ sudo setenforce 0
 sudo chmod -R 755 /opt
 sudo cp /tmp/webapp.zip /opt/
 cd /opt || exit
-sudo unzip webapp.zip -d /opt/
+sudo unzip webapp.zip -d /opt/webapp
 cd /opt/webapp
 sudo npm install
