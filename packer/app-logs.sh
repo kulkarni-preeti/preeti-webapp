8f7a2b64594109fe9de5c20e029d087b4db791da
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-03-17 22:53:13 -0400

    added scripts for config.yaml

--- packer/app-logs.sh
+++ packer/app-logs.sh
@@ -1,2 +1,21 @@
-sudo vi /etc/google-cloud-ops-agent/config.yaml
+cat << EOF | sudo vi /etc/google-cloud-ops-agent/config.yaml
+logging:
+  receivers:
+    webapp-receiver:
+      type: files
+      include_paths:
+        - /var/webapp.log
+      record_log_file_path: true
+  processors:
+    webapp-processor:
+      type: parse_json
+      time_key: time
+      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
+  service:
+    pipelines:
+      default_pipeline:
+        receivers: [webapp-receiver]
+        processors: [webapp-processor]
+EOF
+
 sudo systemctl restart google-cloud-ops-agent
