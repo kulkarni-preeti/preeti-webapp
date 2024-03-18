43c49d8ba1b4ecc89fd52fb17cfc04d129617f20
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-03-18 00:46:45 -0400

    env based log location added

--- logger/logging.js
+++ logger/logging.js
@@ -1,10 +1,17 @@
 import { createLogger, transports, format } from 'winston';
+import * as envType from '../tests/create-account.test.js';
+
+if (envType === "testing") {
+  logFileLoc = './test-logging'
+} else {
+  logFileLoc = '/var/logs/webapp.log'
+}
 
 const logger = createLogger({
   level: 'info',
   format: format.json(),
   transports: [
-    new transports.File({ filename: '/var/logs/webapp.log' })
+    new transports.File({ filename: logFileLoc })
   ]
 });
 
