a6bb4d9762b31b1126070823be102d1729711adc
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-03-18 15:11:50 -0400

    Changed sudo commands

--- packer/create-log.sh
+++ packer/create-log.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ "$(id -u)" != "0" ]; then
-    echo "This script must be run with sudo." 1>&2
-    exit 1
-fi
-
 sudo chmod -R 755 /var
-
 sudo mkdir -p /var/logs
 sudo chmod -R 755 /var/logs
 
