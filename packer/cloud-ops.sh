e8d02540c472735eb8f401e5fe5243173ea22d0c
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-03-17 22:46:52 -0400

    cloud-ops scripts

--- packer/cloud-ops.sh
+++ packer/cloud-ops.sh
@@ -1,6 +1,2 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-
-sudo systemctl status google-cloud-ops-agent"*"
-sudo systemctl start google-cloud-ops-agent"*"
-sudo systemctl enable google-cloud-ops-agent"*"
