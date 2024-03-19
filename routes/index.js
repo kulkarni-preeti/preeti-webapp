ce960ffcd3043d5c2a4742a0d6b39df43c471eea
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-03-19 14:06:22 -0400

    Added logs wherever necessary

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ import healthzRouter from './healthz-router.js'
 import userRouter from './self-router.js';
 import { setErrorResponse } from '../controller/response-handler.js';
 import { authPostgres } from '../authenticate/auth.js'
+import { getLogger } from './../logger/logging.js';
+
+const logger = getLogger();
 
 const isGetMethod = (request, response, next) => {
     if ((request.method !== 'GET') || (Object.keys(request.query).length > 0)) {
+        logger.error('Invalid request. Check Payload', {status: "error"})
         setErrorResponse('405', response);
         return;
     }
