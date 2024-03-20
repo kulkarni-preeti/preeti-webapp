137c1c317229188f6b7afac1db17ff5a69945742
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-03-19 21:59:21 -0400

    healthz logs

--- controller/healthz-controller.js
+++ controller/healthz-controller.js
@@ -7,15 +7,15 @@ const logger = getLogger();
 export const getHealthz = async (request, response) => {
     try {
         if (Object.keys(request.body).length !== 0) {
-            logger.error('Provided request for Healthz API is invalid', {status: "error"})
+            logger.error('Provided request for Healthz API is invalid', {severity: "error", status: "error"})
             setErrorResponse('400', response);
             return;
         } else {
             await authPostgres(response);
-            logger.info('Connecting to sequelize database', {status: "success"})
+            logger.info('Connecting to sequelize database', {severity: "info", status: "success"})
         }
     } catch {
-        logger.error('Internal Service Error. Try again after sometime', {status: "error"})
+        logger.error('Internal Service Error. Try again after sometime', {severity: "error", status: "error"})
         setErrorResponse('500', response);
     }
 };
