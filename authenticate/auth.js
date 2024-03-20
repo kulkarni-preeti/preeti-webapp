7164b8d462f0c6a026e74c5336b8bcc329ab2467
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-03-20 18:32:09 -0400

    added debug logs

--- authenticate/auth.js
+++ authenticate/auth.js
@@ -16,12 +16,13 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
 
 export const authPostgres = async (response) => {
     try {
+        logger.debug('authPostgres is being processed', {severity: "debug"})
         await sequelize.authenticate();
         response.header('Cache-Control', 'no-cache');  // https://www.rfc-editor.org/rfc/rfc9111#section-5.2
-        logger.info('Successfully connected to the database', {status: "success"})
+        logger.info('Successfully connected to the database', {severity: "info"})
         setResponse('200', response);
     } catch (error) {
-        logger.error('Unable to connect to database', {status: "error"})
+        logger.error('Unable to connect to database', {severity: "info"})
         setErrorResponse('503', response);
     }
 }
--- controller/healthz-controller.js
+++ controller/healthz-controller.js
@@ -6,16 +6,18 @@ const logger = getLogger();
 
 export const getHealthz = async (request, response) => {
     try {
+        logger.debug('getHealthz is being processed', {severity: "debug"})
+
         if (Object.keys(request.body).length !== 0) {
-            logger.error('Provided request for Healthz API is invalid', {severity: "error", status: "error"})
+            logger.error('Provided request for Healthz API is invalid', {severity: "error"})
             setErrorResponse('400', response);
             return;
         } else {
             await authPostgres(response);
-            logger.info('Connecting to sequelize database', {severity: "info", status: "success"})
+            logger.info('Connecting to sequelize database', {severity: "info"})
         }
     } catch {
-        logger.error('Internal Service Error. Try again after sometime', {severity: "error", status: "error"})
+        logger.error('Internal Service Error. Try again after sometime', {severity: "error"})
         setErrorResponse('500', response);
     }
 };
